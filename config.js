f467dc1b9a091e4627603aba1653824f076d586d
unisoviet123 <108256002+unisoviet123@users.noreply.github.com>
2022-06-28 13:29:33 +0700

    Update config.js

--- config.js
+++ config.js
@@ -10,27 +10,27 @@ global.autoReadAll = true
 //Silahkan sesuaikan sendiri ya
 
 // Ganti Sewajarnya
-global.owner = ['6285878313791']
-global.pengguna = 'KiZakiXD'
+global.owner = ['62881026320119']
+global.pengguna = 'Lenin'
 global.premium = []
 global.author = 'KiZakiXD'
-global.pemilik = ['6285878313791'] 
-global.ownernomer = '6285878313791'
-global.ownername = 'KiZakiXD'
-global.botname = 'Chitanda - MD'
+global.pemilik = ['62881026320119'] 
+global.ownernomer = '62881026320119'
+global.ownername = 'Lenin'
+global.botname = 'USSR - MD'
 global.tengah = "									"
 global.Prefix = 'Multi Prefix'
-global.meki = '6285878313791'
-global.footer = '𝘊 𝘩 𝘪 𝘵 𝘢 𝘯 𝘥 𝘢 - 𝘔 𝘋'
+global.meki = '62881026320119'
+global.footer = 'Lenin - MD'
 //Isi Sesuai Namalu Buat Tambah Tqtq
-global.namalu = 'Namalu' //Ubah nama kalian buat nambahin di tqtq bot
+global.namalu = 'Lenin' //Ubah nama kalian buat nambahin di tqtq bot
 global.footer1 = 'Jangan Spam Bot.'
 global.footer2 = 'Jika Menemukan Bug Lapor Ke Owner.'
 global.yt = 'https://www.youtube.com/channel/UCHdipvk52k43KsPRenGpD0w' //Jika tidak ada yt jangan diubah buat promosi owner xixixixi
 global.ig = 'https://instagram.com/iamkizakixd'
 global.gc = 'https://chat.whatsapp.com/IJ5ull4HXaY647D6ACw4kj' //join ya kadang kadang pada eval bot disitu lumayan dapet fitur baru wkwkwk
-global.dana = '00000'
-global.gopay = '0000'
+global.dana = '62881026320119'
+global.gopay = '62881026320119'
 global.wame = 'wa.me/6285878313791'
 global.email = 'helloiamkizakixd@gmail.com'
 global.region = 'Indonesia'
